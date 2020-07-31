09f4b7aebf8133617daf5155be5ced7a6ca5f663
Anna <anna@Annas-MacBook-Pro.local>
2020-07-30 22:04:49 -0500

    revised and got blockchain test transaction to work. continue on ethereum items for geth

--- wallet/wallet.py
+++ wallet/wallet.py
@@ -6,7 +6,9 @@ import subprocess
 from dotenv import load_dotenv
 import bit
 import web3
+from web3.middleware import geth_poa_middleware
 from bit import Key
+from bit.network import NetworkAPI
 from web3 import Web3
 from eth_account import Account
 from pathlib import Path
@@ -14,6 +16,7 @@ from getpass import getpass
 import json
 #set for local network
 w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:8545"))
+w3.middleware_onion.inject(geth_poa_middleware, layer=0)
 
 #import mnemnic from environmental variables
 load_dotenv()
@@ -53,17 +56,21 @@ def derive_wallets(mnemonic,coin_list):
 
 #run the function on mneumonic and coins list defined
 output=derive_wallets(mnemonic, coin_list)
+
+
 #print(output_json)
 #output the json format to a json file for reference
 #with open("output.json", "w") as f:
  #   f.write(json.dumps(output))
 
-#grab private key values
+#grab private key values to check output data reading
 #btc-test private key
 btc_priv_key=output['btc-test'][0]['privkey']
 
 #eth priv_key 
 eth_priv_key=output['eth'][0]['privkey']
+
+
 
 
 #function for priv_key_to_account
@@ -77,21 +84,27 @@ def priv_key_to_account(coin,priv_key):
         return account
 
 
+#get bitcion test net address for 2 accounts
+btc_account1=priv_key_to_account('btc-test',output['btc-test'][0]['privkey'])
+btc_account2=priv_key_to_account('btc-test',output['btc-test'][1]['privkey'])
 
-#address2=output['eth'][1]['address']
+#get ethereum test address for 2 accounts
+eth_account1=priv_key_to_account('eth',output['eth'][0]['privkey'])
+eth_account2=priv_key_to_account('eth',output['eth'][1]['privkey'])
+
 
 #function to create raw transaction with all metadata to transact
 def create_tx(coin,account,to,amount):
     if coin=='eth':
         gasEstimate=w3.eth.estimateGas({
             "from":account.address,
-            "to":to,
+            "to":to.address,
             "value":amount
         })
         
         eth_info={
             "from":account.address,
-            "to":to,
+            "to":to.address,
             "value":amount,
             "gas": gasEstimate,
             "gasPrice":w3.eth.gasPrice,
@@ -99,7 +112,7 @@ def create_tx(coin,account,to,amount):
         }
         return eth_info
     elif coin=='btc-test':
-        return bit.PrivateKeyTestnet.prepare_transaction(account.address,[(to,amount,BTC)])
+        return bit.PrivateKeyTestnet.prepare_transaction(account.address,[(to.address,amount,BTC)])
 
 
 #function to sign the transaction and send on designated network
@@ -115,8 +128,8 @@ def send_tx(coin,account,to,amount):
         #get raw transaction data
         raw_tx=create_tx(coin,account,to,amount)
         #sign the transaction
-        signed=bit.multisig1.sign_transaction(raw_tx)
+        signed=account.sign_transaction(raw_tx)
         #send the transaction
-        return bit.NetworkAPI.broadcast_tx_testnet(signed)
+        return NetworkAPI.broadcast_tx_testnet(signed)
 
 
